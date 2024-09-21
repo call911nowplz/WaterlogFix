c46b620f1212f00f8ba5ba463cdcd90ef916f011
Call911 <ipvplikeagod@gmail.com>
2024-09-21 18:38:47 +0100

    1.21 support & fix Y level below 0 bug

--- src/main/java/pro/dracarys/WaterlogFix/WaterlogFix.java
+++ src/main/java/pro/dracarys/WaterlogFix/WaterlogFix.java
@@ -25,23 +25,7 @@ public class WaterlogFix extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        try {
-            int pluginId = 7322;
-            new Metrics(this, pluginId);
-        } catch (Exception ex) {
-            getServer().getLogger().log(Level.SEVERE, "Error while trying to register Metrics (bStats)");
-        }
         plugin = this;
-        checkServerVersion();
-        if (getServerVersion() < 13) {
-            Util.sendConsole("&cYour Server version (1." + ver + ") is not compatible with this plugin! Only 1.13+ supported! Disabling...");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        } else if (getServerVersion() > 17) {
-            Util.sendConsole("&e[WaterlogFix] Your Server version (1." + ver + ") should be compatible but it's untested!");
-        } else {
-            Util.sendConsole("&a[WaterlogFix] Your Server version (1." + ver + ") is compatible");
-        }
         saveDefaultConfig();
         File configFile = new File(getDataFolder(), "config.yml");
         try {
@@ -84,26 +68,13 @@ public class WaterlogFix extends JavaPlugin {
     }
 
     private void printPluginInfo() {
-        Util.sendConsole("&f<> &f<> &f<> &f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<> &f<> &f<> &f<>");
         Util.sendConsole(" ");
         Util.sendConsole("&f-  &c" + getDescription().getName() + " &7v" + getDescription().getVersion() + "&a Enabled");
-        Util.sendConsole("&f-  &f&o" + getDescription().getDescription());
-        Util.sendConsole("&f- &eMade with &4♥ &eby &f" + getDescription().getAuthors().get(0));
-        if (getDescription().getVersion().contains("-DEV"))
-            Util.sendConsole("&f- &cThis is a DEV BUILD, report any unexpected behaviour to the Author!");
+        Util.sendConsole("&f- &eMade &eby &f" + getDescription().getAuthors().get(0) + "&eMaintained By &fCall911");
         Util.sendConsole(" ");
-        Util.sendConsole("&f<> &f<> &f<> &f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<>&f<> &f<> &f<> &f<>");
     }
 
-    private static int ver;
 
-    private static void checkServerVersion() {
-        ver = Integer.parseInt(Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3].replace("1_", "").substring(1).replaceAll("_R\\d", ""));
-    }
-
-    private static int getServerVersion() {
-        return ver;
-    }
 
     public static String getMessage(String key) {
         return Util.color(plugin.getConfig().getString("Messages." + key, "Missing Translation for key " + key));
